1724614a190c8f8b211d946d829cb8cda8f668e4
tomasVis <>
2019-08-31 18:37:37 +0300

    modules

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
-
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser')
 
 var cors = require('cors');
 
@@ -12,14 +12,29 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
- mongoose.connect(process.env.MONGOLAB_URI);
+// mongoose.connect(config.dbUrl);
+//"mongodb://localhost:27017/nodeDB"
+mongoose.connect("mongodb://localhost:27017/nodeDB", {useNewUrlParser: true});
+//mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+});
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use("/name", bodyParser.urlencoded({extended: false}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/name', function(req, res) {
+  
+  console.log(req.body);
+  res.send({ name: req.query.first+" "+req.query.last});
+  });
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
@@ -33,5 +48,5 @@ app.get("/api/hello", function (req, res) {
 
 
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log('Node.js listening on port '+port);
 });
