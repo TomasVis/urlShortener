3945e690b56c0ccff55f2919658c5f368489e5e5
tomasVis <>
2019-09-06 15:43:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
 
-mongoose.connect("mongodb://localhost:27017/a" || process.env.MONGOLAB_URI, {useNewUrlParser: true});
-//mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost:27017/a" , {useNewUrlParser: true});
+mongoose.connect(process.env.MONGOLAB_URI, {useNewUrlParser: true});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
